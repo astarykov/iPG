0503d1798b1a982ed349072472a082494347541a
Starykov Anton <starykov.anton@apriorit.com>
2017-04-22 14:15:04 +0300

    empty project

---
+++ iPhotoGalery/iPhotoGalery/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iPhotoGalery
+//
+//  Created by admin on 4/22/17.
+//  Copyright © 2017 staryaprod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iPhotoGalery/iPhotoGalery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhotoGalery
+//
+//  Created by admin on 4/22/17.
+//  Copyright © 2017 staryaprod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
